e851496b9e2ab9ebbabf1088e30058b9833ebec6
Michael Mattice <mmattice@ok7-eleven.com>
2016-05-02 16:00:22 -0500

    add argument handling tests

--- test/test_meta.py
+++ test/test_meta.py
@@ -10,5 +10,20 @@ class TestAvailability(unittest.TestCase):
         a = AmountOther()
         self.assertEquals(a._tagid, '9F03')
 
+
+class TestArguments(unittest.TestCase):
+    def test_default(self):
+        a = AmountOther('abcd')
+        self.assertEquals(repr(a), "AmountOther(hexvalue='abcd')")
+
+    def test_hexvalue(self):
+        a = AmountOther(hexvalue='abcd')
+        self.assertEquals(repr(a), "AmountOther(hexvalue='abcd')")
+
+    def test_value(self):
+        a = AmountOther(value='abcd')
+        self.assertEquals(repr(a), "AmountOther(hexvalue='61626364')")
+
+
 if __name__ == '__main__':
     unittest.main()
