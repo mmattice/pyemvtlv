0b1d3bb25734e04d17af0961c5f5bad5adb4b9cd
Michael Mattice <mmattice@ok7-eleven.com>
2016-05-02 16:01:35 -0500

    comment TagModule

--- pyemvtlv/types/tags/__init__.py
+++ pyemvtlv/types/tags/__init__.py
@@ -8,12 +8,18 @@ class BaseTag(object):
 
 class TagModule(object):
     def __init__(self, doftags):
+        # store the tags, jic
         self.__doftags = doftags
+        # define the list of exported attributes
         self.__all__ = list(doftags.iterkeys())
+        # create our classes
         for item in doftags:
             self.__dict__[item] = self.createTagClass(item, doftags[item])
 
     def createTagClass(self, name, tagid):
+        """
+        Creates a class named `name` with a tagid of `tagid`
+        """
         return type(name, (BaseTag, ), {"_tagid": tagid})
 
 sys.modules[__name__] = TagModule(doftags)
