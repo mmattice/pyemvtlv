9d102466ee9a49bca2823037c425bdf53694aa83
Michael Mattice <mmattice@ok7-eleven.com>
2016-05-03 17:11:11 -0500

    add install_requires

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(name='pyemvtlv',
       version='0.1.0',
       setup_requires=["setuptools_git >= 0.3", ],
+      install_requires=["future >= 0.15.2", ],
       description='Python EMV TLV Encoder/decoder',
       author='Mike Mattice',
       author_email='mike.mattice@gmail.com',
