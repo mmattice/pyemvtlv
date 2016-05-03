0433349b6299aad714a3c757242311171859e863
Michael Mattice <mmattice@ok7-eleven.com>
2016-05-02 23:15:59 -0500

    get basic sdists working

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='pyemvtlv',
-      version='0.01',
+      version='0.1.0',
+      setup_requires=["setuptools_git >= 0.3",],
       description='Python EMV TLV Encoder/decoder',
       author='Mike Mattice',
       author_email='mike.mattice@gmail.com',
       url='https://github.com/mmattice/pyemvtlv/',
-      packages=['pyemvtlv'],
+      packages=find_packages(),
+      include_package_data=True,
       )
