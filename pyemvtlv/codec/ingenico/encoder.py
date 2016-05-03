ce9ef03fafd83746fd361297b54db959d6e4ada3
Michael Mattice <mmattice@ok7-eleven.com>
2016-05-03 13:36:47 -0500

    add ingenico encoder

---
+++ pyemvtlv/codec/ingenico/encoder.py
@@ -0,0 +1,25 @@
+from binascii import hexlify
+
+FS = '\x1c'
+
+
+def printable(s):
+    for c in s:
+        if not (c.isalnum() or c.isspace()):
+            return False
+    return True
+
+
+class Encoder(object):
+    def __call__(self, tag):
+        tagId = tag._tagid
+        l = len(tag._value)
+        ls = "%4.4X" % (l,)
+        if l < 256:
+            ls = ls[2:]
+        return 'T{}:{}:h{}{}'.format(tagId,
+                                     ls,
+                                     hexlify(tag._value).upper(),
+                                     FS)
+
+encode = Encoder()
---
+++ test/test_encode_ingenico.py
@@ -0,0 +1,28 @@
+from pyemvtlv.codec.binary.decoder import decode as pber_dec
+from pyemvtlv.codec.ingenico.encoder import encode as ing_enc
+import unittest
+
+from base64 import b64decode
+
+
+class IssuerScriptEncoderTestCase(unittest.TestCase):
+    def setUp(self):
+        ber = b64decode('kQqQyoq7/uC+JAAScR6fGAQAAAABhhWEJAACECLDCJ81BkwUJEfxH6LsCqGKAjAw')
+        el = []
+        while ber:
+            y, ber = pber_dec(ber)
+            if y:
+                el.append(y)
+        self.el = el
+
+    def testEncodeIssuerScript(self):
+        dr = ['T91:0A:h90CA8ABBFEE0BE240012\x1c',
+              'T71:1E:h9F1804000000018615842400021022C3089F35064C142447F11FA2EC0AA1\x1c',
+              'T8A:02:h3030\x1c']
+        tlvs = map(ing_enc, self.el)
+
+        self.assertEqual(dr, tlvs)
+
+
+if __name__ == '__main__':
+    unittest.main()
